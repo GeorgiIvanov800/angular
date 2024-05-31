0f596333493ca9f9629a88459b37c396a56195e3
Georgi Ivanov <Georgi_Ivanov92@pm.me>
2024-05-31 18:24:29 +0200

    added loader

--- Angular/04.WorkshoComponents/workshop-components/src/app/shared/shared.module.ts
+++ Angular/04.WorkshoComponents/workshop-components/src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoaderComponent
+  ],
   imports: [CommonModule],
 })
 export class SharedModule {}
