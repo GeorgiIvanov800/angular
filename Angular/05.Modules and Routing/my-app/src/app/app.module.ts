0979c6f27daee3ed621d473357d76582c1298053
Georgi Ivanov <Georgi_Ivanov92@pm.me>
2024-06-02 13:41:24 +0200

    add spinner

--- Angular/05.Modules and Routing/my-app/src/app/app.module.ts
+++ Angular/05.Modules and Routing/my-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
