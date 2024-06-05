02a617f2b8f7a9583b5e57d15692012059c230c4
Georgi Ivanov <Georgi_Ivanov92@pm.me>
2024-06-05 20:19:18 +0200

    current theme implemented

--- Angular/06.WorkshopModulesAndRouting/workshop-components/src/app/theme/current-theme/current-theme.component.ts
+++ Angular/06.WorkshopModulesAndRouting/workshop-components/src/app/theme/current-theme/current-theme.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Theme } from 'src/app/types/theme';
 
 @Component({
   selector: 'app-current-theme',
   templateUrl: './current-theme.component.html',
-  styleUrls: ['./current-theme.component.css']
+  styleUrls: ['./current-theme.component.css'],
 })
-export class CurrentThemeComponent {
+export class CurrentThemeComponent implements OnInit {
+  theme = {} as Theme;
 
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    // console.log('on init');
+
+    this.api.getTheme('5fa64b6f2183ce1728ff371e').subscribe((theme) => {
+      this.theme = theme;
+      console.log(theme);
+    });
+  }
 }
