60446eb438a6bc72baf18aaba9df29247462573f
Georgi Ivanov <Georgi_Ivanov92@pm.me>
2024-06-09 18:40:49 +0200

    added mouse event

--- Angular/07.DirectivrsAndForms/directives-and-forms/src/app/highlight.directive.ts
+++ Angular/07.DirectivrsAndForms/directives-and-forms/src/app/highlight.directive.ts
@@ -5,12 +5,33 @@ import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 })
 export class HighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
   ngOnInit(): void {
     console.log(this.elRef.nativeElement);
-    // Don't use this way not recommended
+    // It's not a good practice to directly access DOM elements via ElementRef
     // this.elRef.nativeElement.style.background = 'orange';
 
-    // use renderer
-    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'purple');
+    // Use Renderer2 to manipulate DOM elements -> Good pracitce
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background', 'lightblue');
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseenter',
+      this.mousEneterHandler.bind(this)
+    );
+
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaveHandler.bind(this)
+    );
+  }
+
+  mousEneterHandler(e: MouseEvent): void {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'lightblue');
+  }
+
+  mouseLeaveHandler(e: MouseEvent): void {
+    this.renderer.removeStyle(this.elRef.nativeElement, 'background');
   }
 }
