4a45d79fa399bb2fb5ece17b84a01f96e92a9516
Georgi Ivanov <Georgi_Ivanov92@pm.me>
2024-06-09 18:55:27 +0200

    created new components

--- Angular/07.DirectivrsAndForms/directives-and-forms/src/app/highlight.directive.ts
+++ Angular/07.DirectivrsAndForms/directives-and-forms/src/app/highlight.directive.ts
@@ -1,13 +1,22 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective implements OnInit {
+  @HostListener('mouseover', ['$event']) mouseOverHandler(e: MouseEvent) {
+    console.log('mouseOver ', e);
+  }
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    console.log(this.elRef.nativeElement);
+    // console.log(this.elRef.nativeElement);
     // It's not a good practice to directly access DOM elements via ElementRef
     // this.elRef.nativeElement.style.background = 'orange';
 
@@ -28,10 +37,18 @@ export class HighlightDirective implements OnInit {
   }
 
   mousEneterHandler(e: MouseEvent): void {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'lightblue');
+    /* Setting styles */
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background', 'lightblue');
+
+    /** Setting classes */
+    this.renderer.addClass(this.elRef.nativeElement, 'highlight');
   }
 
   mouseLeaveHandler(e: MouseEvent): void {
-    this.renderer.removeStyle(this.elRef.nativeElement, 'background');
+    /* Setting styles */
+    // this.renderer.removeStyle(this.elRef.nativeElement, 'background');
+
+    /** Setting classes */
+    this.renderer.removeClass(this.elRef.nativeElement, 'highlight');
   }
 }
