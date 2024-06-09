fd77cd704be1f742c6be54dab901b1cb1b246b07
Georgi Ivanov <Georgi_Ivanov92@pm.me>
2024-06-09 14:43:43 +0200

    start using HighlightDirective

---
+++ Angular/07.DirectivrsAndForms/directives-and-forms/src/app/highlight.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective implements OnInit {
+  constructor(private elRef: ElementRef) {}
+  ngOnInit(): void {
+    console.log(this.elRef.nativeElement);
+  }
+}
