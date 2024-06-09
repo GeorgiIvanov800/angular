35e82ac74834d74a5481cf1af9e18aad9525af15
Georgi Ivanov <Georgi_Ivanov92@pm.me>
2024-06-09 14:50:23 +0200

    add Renderer2

--- Angular/07.DirectivrsAndForms/directives-and-forms/src/app/highlight.directive.ts
+++ Angular/07.DirectivrsAndForms/directives-and-forms/src/app/highlight.directive.ts
@@ -1,11 +1,16 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
 export class HighlightDirective implements OnInit {
-  constructor(private elRef: ElementRef) {}
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
   ngOnInit(): void {
     console.log(this.elRef.nativeElement);
+    // Don't use this way not recommended
+    // this.elRef.nativeElement.style.background = 'orange';
+
+    // use renderer
+    this.renderer.setStyle(this.elRef.nativeElement, 'background', 'purple');
   }
 }
